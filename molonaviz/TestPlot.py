a906b02516737a78ec1009ff0604989576348c35
cdemaillynesle <charlotte.de_mailly_nesle@mines-paristech.fr>
2021-04-16 10:16:53 +0200

    Petites updates

--- molonaviz/TestPlot.py
+++ molonaviz/TestPlot.py
@@ -1,10 +1,11 @@
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
 from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys  # We need sys so that we can pass argv to QApplication
 import os
 import pandas as pd
 from usefulfonctions import time_convert
+from datetime import datetime
 
 dftemp = pd.read_csv("/Users/charlottedemaillynesle/Desktop/ÉTUDE QUI VA MARCHER/Point 1/raw_data/raw_temperatures.csv", index_col=0)
 #print(dftemp)
@@ -16,7 +17,7 @@ Tempcapteur1 = dftemp[dftemp.columns[0]].values.tolist()
 
 #VOIR https://stackoverflow.com/questions/49046931/how-can-i-use-dateaxisitem-of-pyqtgraph
 
-
+"""
 class MainWindow(QtWidgets.QMainWindow):
     
     def __init__(self, *args, **kwargs):
@@ -31,13 +32,26 @@ class MainWindow(QtWidgets.QMainWindow):
         # plot data: x, y values
         self.graphWidget.plot(Timeint, Tempcapteur1)
 
+"""
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(app.exec_())
+class TimeAxisItem(pg.AxisItem):
+    def tickStrings(self, values, scale, spacing):
+        return [datetime.fromtimestamp(value) for value in values]
 
+list_x = Timeint
+
+list_y = Tempcapteur1
+
+app = QtGui.QApplication([])
+
+
+date_axis = TimeAxisItem(orientation='bottom')
+graph = pg.PlotWidget(axisItems = {'bottom': date_axis})
+
+graph.plot(x=[x.timestamp() for x in list_x], y=list_y, pen=None, symbol='o')
+graph.show()
 
 if __name__ == '__main__':
-    main()
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
