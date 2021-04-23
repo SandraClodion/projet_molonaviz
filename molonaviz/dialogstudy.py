553689d2fa72d2f08e27814a9343f4d579a64213
SandraClodion <clodionsandra@gmail.com>
2021-04-23 09:58:33 +0200

    gestion des erreurs fonction createStudy()

--- molonaviz/dialogstudy.py
+++ molonaviz/dialogstudy.py
@@ -1,7 +1,7 @@
-import sys
-import os
+import sys, os, errno
 from PyQt5 import QtWidgets, uic
 from study import Study
+from errors import *
 
 From_DialogStudy = uic.loadUiType(os.path.join(os.path.dirname(__file__),"dialogstudy.ui"))[0]
 
@@ -28,11 +28,23 @@ class DialogStudy(QtWidgets.QDialog, From_DialogStudy):
             self.lineEditSensorsDir.setText(dirPath) 
 
     def setStudy(self):
+
         name = self.lineEditName.text()
+        if not name: #si la lineEdit est vide
+            raise EmptyFieldError('New study should have a name')
+        
         sensorsdir = self.lineEditSensorsDir.text()
+        if not os.path.isdir(sensorsdir):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), sensorsdir)
+        
         rootpath = self.lineEditRootDirPath.text()
+        if not os.path.isdir(rootpath):
+            raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), rootpath)
+            
         rootdir = os.path.join(rootpath, name)
         os.mkdir(rootdir)
         return Study(name, rootdir, sensorsdir)
+
+            
 
 
